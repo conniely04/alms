abb58432d744f597040647481884025fb376e9f4
conniely <conniely04@gmail.com>
2024-04-06 12:55:49 -0700

    get user location

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -15,6 +15,17 @@ function Map() {
       zoom: 12.5,
     });
 
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true,
+      })
+    );
     // Clean up on unmount
     return () => map.remove();
   }, []); // Empty dependency array means this effect runs once on mount and clean up on unmount
