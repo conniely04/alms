2592de3c4d89c408244e656762e14185c9b0716c
conniely <conniely04@gmail.com>
2024-04-06 10:44:15 -0700

    mapbox component

---
+++ client/src/components/Gpt.jsx
@@ -0,0 +1,16 @@
+function GPTComponent() {
+  return (
+    <div>
+      <h1>Embedded Content</h1>
+      <iframe
+        src="https://chat.openai.com/gpts/editor/g-WDaNMjBT1"
+        width="500px"
+        height="600px"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
+
+export default GPTComponent;
