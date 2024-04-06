40610d371c55af63dc9921ba75336350bec3f3d0
conniely <conniely04@gmail.com>
2024-04-06 11:30:42 -0700

    map/gpt component

---
+++ client/src/components/Prompt.jsx
@@ -0,0 +1,13 @@
+function Prompt() {
+  return (
+    <>
+      <iframe
+        src="https://chat.openai.com/g/g-WDaNMjBT1-story-weaver"
+        width="700px"
+        height="700px"
+      ></iframe>
+    </>
+  );
+}
+
+export default Prompt;
