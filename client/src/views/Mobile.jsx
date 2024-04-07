774e60e285489cf33aad1fc4712b4c7a43517e5b
Alex Bui <alexnbui@gmail.com>
2024-04-06 17:24:38 -0700

    redid css

---
+++ client/src/views/Mobile.jsx
@@ -0,0 +1,27 @@
+import Prompt from '../components/Prompt'
+import Map from '../components/Map'
+import { useState } from 'react'
+import { IoMdMap, IoIosChatboxes } from "react-icons/io";
+
+export default function Web() {
+
+    const [view, setView] = useState('chat')
+
+    function toggleView() {
+        setView(view === 'chat' ? 'map' : 'chat')
+    }
+
+    return (
+        <>
+            <div className="relative flex flex-col h-full w-full p-[5%]">
+                <div className="flex justify-between pb-5 border-b-2 border-white">
+                    <div className='my-auto'>alms</div>
+                    <div className='top-0 right-0 p-2 bg-slate-800 rounded-md' onClick={toggleView}>{
+                        view === 'chat' ? <IoMdMap size={20} /> : <IoIosChatboxes size={20} />
+                    }</div>
+                </div> {/* TODO: CHANGE FONT */}
+                {view === 'chat' ? <Prompt /> : <Map />}
+            </div>
+        </>
+    )
+}
---
+++ client/src/views/Web.jsx
@@ -0,0 +1,19 @@
+import Prompt from '../components/Prompt'
+import Map from '../components/Map'
+
+export default function Web() {
+
+    return (
+        <>
+            <div className='relative h-full w-full'>
+                <div className='flex justify-between p-5 border-b-2 border-white'>
+                    <div className='my-auto'>alms</div>
+                </div>
+                <div className='absolute p-5 left-[5%] bottom-[10%] bg-slate-900 overflow-hidden rounded-2xl border-2 h-fit max-h-96'>
+                    <Prompt />
+                </div>
+                <Map />
+            </div>
+        </>
+    )
+}
