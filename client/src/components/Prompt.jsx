943dfd996ad5cce1f714578fed436e0daac5ea9c
conniely <conniely04@gmail.com>
2024-04-07 02:11:24 -0700

    stuck on array thing

--- client/src/components/Prompt.jsx
+++ client/src/components/Prompt.jsx
@@ -1,76 +1,94 @@
 import React, { useState } from "react";
 
-export default function Prompt({messages, setMessages, longitude, latitude}) {
+export default function Prompt({
+  messages,
+  setMessages,
+  longitude,
+  latitude,
+  onDataAvailable,
+}) {
+  const [inputText, setInputText] = useState("");
 
-    const [inputText, setInputText] = useState("");
-
-    function sendMessage() {
-        if (inputText === "") {
-            return;
-        }
-        const newMessages = [...messages, { "role": "user", "content": inputText }];
-        setMessages(newMessages);
-        setInputText("");
-
-        fetch("http://localhost:8000/api/v1/chat/", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                "messages": newMessages,
-                "longitude": longitude, // TO BE CHANGED
-                "latitude": latitude, // TO BE CHANGED
-                "radius": 0.005, // TO BE CHANGED
-            })
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            setMessages([...newMessages, { "role": "assistant", "content": data.message }]);
-        })
-        .catch((error) => {
-            console.error("Error:", error);
-        })
+  function sendMessage() {
+    if (inputText === "") {
+      return;
     }
+    const newMessages = [...messages, { role: "user", content: inputText }];
+    setMessages(newMessages);
+    setInputText("");
 
-    return (
-        <div className="h-full flex flex-col">
-            <div className="flex flex-col overflow-y-scroll flex-1 gap-3 py-5 px-3 no-scrollbar">{
-                messages.slice(1,).map((message) => {
-                    return (
-                        message.role === "assistant"
-                            ?
-                            <div className="flex">
-                                <div className="flex flex-col flex-1 gap-2">
-                                    <div className="text-slate-400 px-1">alms</div>
-                                    <div className="bg-slate-800 px-3 py-2 rounded-lg max-w-[80%] w-fit break-words">{message.content}</div>
-                                </div>
-                            </div>
-                            :
-                            <div className="flex flex-row-reverse">
-                                <div className="flex flex-col max-w-[80%] gap-2">
-                                    <div className="text-slate-400 text-right px-1">you</div>
-                                    <div className="bg-orange-600 px-3 py-2 rounded-lg w-fit break-words">{message.content}</div>
-                                </div>
-                            </div>
-                    )
-                })
-            } </div>
-            <div className="flex flex-row gap-5 border-t-2 pt-5">
-                <input
-                    className="flex-1 h-12 rounded-lg p-3 bg-transparent border-2 border-white bg-slate-800"
-                    type="text"
-                    placeholder="Type a message..."
-                    value={inputText}
-                    onChange={(e) => { setInputText(e.target.value) }}
-                    onKeyDown={(e) => {
-                        if (e.key === "Enter") {
-                            sendMessage();
-                        }
-                    }}
-                />
-                <div className="m-auto bg-orange-600 px-3 py-2 rounded-md" onClick={sendMessage}>Send</div>
+    fetch("http://localhost:8000/api/v1/chat/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        messages: newMessages,
+        longitude: longitude, // TO BE CHANGED
+        latitude: latitude, // TO BE CHANGED
+        radius: 0.005, // TO BE CHANGED
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        //console.log(data);
+
+        setMessages([
+          ...newMessages,
+          { role: "assistant", content: data.message },
+        ]);
+        onDataAvailable(data.parking);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+
+  return (
+    <div className="h-full flex flex-col">
+      <div className="flex flex-col overflow-y-scroll flex-1 gap-3 py-5 px-3 no-scrollbar">
+        {messages.slice(1).map((message) => {
+          return message.role === "assistant" ? (
+            <div className="flex">
+              <div className="flex flex-col flex-1 gap-2">
+                <div className="text-slate-400 px-1">alms</div>
+                <div className="bg-slate-800 px-3 py-2 rounded-lg max-w-[80%] w-fit break-words">
+                  {message.content}
+                </div>
+              </div>
             </div>
+          ) : (
+            <div className="flex flex-row-reverse">
+              <div className="flex flex-col max-w-[80%] gap-2">
+                <div className="text-slate-400 text-right px-1">you</div>
+                <div className="bg-orange-600 px-3 py-2 rounded-lg w-fit break-words">
+                  {message.content}
+                </div>
+              </div>
+            </div>
+          );
+        })}{" "}
+      </div>
+      <div className="flex flex-row gap-5 border-t-2 pt-5">
+        <input
+          className="flex-1 h-12 rounded-lg p-3 bg-transparent border-2 border-white bg-slate-800"
+          type="text"
+          placeholder="Type a message..."
+          value={inputText}
+          onChange={(e) => {
+            setInputText(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              sendMessage();
+            }
+          }}
+        />
+        <div
+          className="m-auto bg-orange-600 px-3 py-2 rounded-md"
+          onClick={sendMessage}
+        >
+          Send
         </div>
-    );
-};
-
+      </div>
+    </div>
+  );
+}
--- client/src/views/Web.jsx
+++ client/src/views/Web.jsx
@@ -1,22 +1,43 @@
-import Prompt from '../components/Prompt'
-import Map from '../components/Map'
+import React, { useState } from "react";
+import Prompt from "../components/Prompt";
+import Map from "../components/Map";
 
-export default function Web({messages, setMessages, longitude, setLongitude, latitude, setLatitude}) {
-
-    return (
-        <>
-            <div className='flex flex-col relative h-full w-full'>
-                <div className='flex justify-between p-5 border-b-2 border-white'>
-                    <div className='my-auto'>alms</div>
-                </div>
-                <div className='absolute z-10 p-5 left-[5%] bottom-[10%] bg-slate-900 overflow-hidden rounded-2xl border-2 h-[50%] max-h-96 w-[30%] max-w-[35rem] min-w-[24rem]'>
-                    <Prompt messages={messages} setMessages={setMessages} longitude={longitude} latitude={latitude} />
-                </div>
-                <div className='flex-1'>
-
-                    <Map setLongitude={setLongitude} setLatitude={setLatitude} />
-                </div>
-            </div>
-        </>
-    )
+export default function Web({
+  messages,
+  setMessages,
+  longitude,
+  setLongitude,
+  latitude,
+  setLatitude,
+}) {
+  const [parkingData, setParkingData] = useState([]);
+  const handleDataAvailable = (data) => {
+    console.log("web.jsx:", data);
+    setParkingData([...parkingData, data]);
+  };
+  return (
+    <>
+      <div className="flex flex-col relative h-full w-full">
+        <div className="flex justify-between p-5 border-b-2 border-white">
+          <div className="my-auto">alms</div>
+        </div>
+        <div className="absolute z-10 p-5 left-[5%] bottom-[10%] bg-slate-900 overflow-hidden rounded-2xl border-2 h-[50%] max-h-96 w-[30%] max-w-[35rem] min-w-[24rem]">
+          <Prompt
+            messages={messages}
+            setMessages={setMessages}
+            longitude={longitude}
+            latitude={latitude}
+            onDataAvailable={handleDataAvailable}
+          />
+        </div>
+        <div className="flex-1">
+          <Map
+            setLongitude={setLongitude}
+            setLatitude={setLatitude}
+            parkingData={parkingData}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
