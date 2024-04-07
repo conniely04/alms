1e482bc79b93f2e602f17ae4559bbe1bc1e2e227
conniely <conniely04@gmail.com>
2024-04-06 22:29:54 -0700

    auto trigger

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,59 +1,64 @@
-import React, { useEffect, useRef } from "react"
-import mapboxgl from "mapbox-gl"
-import "mapbox-gl/dist/mapbox-gl.css"
+import React, { useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 mapboxgl.accessToken =
-    "pk.eyJ1IjoiY29ubmllbHkwNCIsImEiOiJjbG5namJ4NTYwdm82MmtxeDVlbjdlbmp4In0.NWMlrVKbeXYxskBZkpQI0Q"
+  "pk.eyJ1IjoiY29ubmllbHkwNCIsImEiOiJjbG5namJ4NTYwdm82MmtxeDVlbjdlbmp4In0.NWMlrVKbeXYxskBZkpQI0Q";
 
 export default function Map() {
+  let dragmarker;
+  const mapContainerRef = useRef(null);
 
-    let dragmarker
-    const mapContainerRef = useRef(null)
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: "mapbox://styles/mapbox/navigation-night-v1",
+      center: [-122.4147, 37.7856],
+      zoom: 12.5,
+    });
+    map.on("load", () => {
+      geolocate.trigger();
+    });
 
-    useEffect(() => {
-        const map = new mapboxgl.Map({
-            container: mapContainerRef.current,
-            style: "mapbox://styles/mapbox/navigation-night-v1",
-            center: [-122.4147, 37.7856],
-            zoom: 12.5,
-        })
+    const geolocate = new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true,
+      },
+      trackUserLocation: true,
+      showUserHeading: true,
+    });
 
-        const geolocate = new mapboxgl.GeolocateControl({
-            positionOptions: {
-                enableHighAccuracy: true,
-            },
-            trackUserLocation: true,
-            showUserHeading: true,
-        })
+    geolocate.on("geolocate", async (e) => {
+      console.log("Geolocated:", e.coords.longitude, e.coords.latitude);
+      const latitude = e.coords.latitude;
+      const longitude = e.coords.longitude;
 
-        geolocate.on("geolocate", async (e) => {
-            console.log("Geolocated:", e.coords.longitude, e.coords.latitude)
-            if (dragmarker) {
-                dragmarker.remove()
-            }
-            if (latitude && longitude) {
-                dragmarker = new mapboxgl.Marker({ offset: [0, -50 / 2] })
-                    .setLngLat([parseFloat(longitude), parseFloat(latitude)])
-                    .setDraggable(true)
-                    .addTo(map)
+      if (dragmarker) {
+        dragmarker.remove();
+      }
+      if (latitude && longitude) {
+        dragmarker = new mapboxgl.Marker({ offset: [0, -50 / 2] })
+          .setLngLat([parseFloat(e.coords.longitude), parseFloat(latitude)])
+          .setDraggable(true)
+          .addTo(map);
 
-                dragmarker.on("dragend", function () {
-                    const lngLat = dragmarker.getLngLat()
-                    console.log(
-                        `New longitude: ${lngLat.lng}, New latitude: ${lngLat.lat}`
-                    )
-                })
-            }
-        })
+        dragmarker.on("dragend", function () {
+          const lngLat = dragmarker.getLngLat();
+          console.log(
+            `New longitude: ${lngLat.lng}, New latitude: ${lngLat.lat}`
+          );
+        });
+      }
+    });
 
-        map.addControl(geolocate)
+    map.addControl(geolocate);
 
-        return () => map.remove()
-    }, [])
+    return () => map.remove();
+  }, []);
 
-    return (
-        <div ref={mapContainerRef} className="w-full h-full">
-            <div ref={mapContainerRef} className="mapContainer" />
-        </div>
-    )
+  return (
+    <div ref={mapContainerRef} className="w-full h-full">
+      <div ref={mapContainerRef} className="mapContainer" />
+    </div>
+  );
 }
